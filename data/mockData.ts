23fe33221124482353d2e72f61c7e73c9916c89e
WidyaSari-page <reallywidya10425@gmail.com>
2026-09-21 10:46:09 +0700

    Change Instagram URL in mockDataganti link ig
    Updated Instagram URL in socialLinks.

--- data/mockData.ts
+++ data/mockData.ts
@@ -87,7 +87,7 @@ export const tracks = [
 ];
 
 export const socialLinks = [
-  { name: 'Instagram', icon: Camera, url: 'http://example.com', color: 'hover:border-blue-500/50 hover:text-blue-500' },
+  { name: 'Instagram', icon: Camera, url: 'https://www.instagram.com/_widyassaaa/', color: 'hover:border-blue-500/50 hover:text-blue-500' },
   { name: 'LinkedIn', icon: Briefcase, url: 'https://www.linkedin.com/in/widya-sari-20b100421/', color: 'hover:border-blue-500/50 hover:text-blue-500' },
   { name: 'GitHub', icon: Code, url: 'https://github.com/WidyaSari-page', color: 'hover:border-blue-500/50 hover:text-blue-500' },
   { name: 'Email', icon: Mail, url: 'mailto:reallywidya10425@gmail.com', color: 'hover:border-blue-500/50 hover:text-blue-500' }
